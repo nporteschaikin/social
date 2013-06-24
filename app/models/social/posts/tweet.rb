20e5a8ea663414fe7c0bd6615652db2ec36d1738
Noah Portes Chaikin <noah@kingandpartners.com>
2013-06-24 15:46:44 -0400

    making 3.2 compatible

--- app/models/social/posts/tweet.rb
+++ app/models/social/posts/tweet.rb
@@ -2,19 +2,18 @@ module Social
 	module Posts
 		class Tweet < ActiveRecord::Base
 			
-			attr_accessible :twitter_id, :tweet
+			attr_accessible :tweet_id, :tweet
 			
-			validates :twitter_id, presence: true
+			validates :tweet_id, presence: true
 			validates :tweet, presence: true
 			
 			include Social::Posts::Create
 
 			def self.import
 				if Social::Engine.config.twitter_enabled
-					client = Twitter::Client.new consumer_key: Social::Engine.config.twitter_consumer_key, consumer_secret: Social::Engine.config.twitter_consumer_secret
-					tweets = client.user_timeline Social::Engine.config.twitter_username
+					tweets = Twitter.user_timeline Social::Engine.config.twitter_username
 					tweets.each do |tweet|
-						self.find_or_create_by_twitter_id(tweet.id) { |p| p.tweet = tweet.text; p.published_at = tweet.created_at }
+						self.find_or_create_by_tweet_id(tweet.id) { |p| p.tweet = tweet.text; p.published_at = tweet.created_at }
 					end
 				else
 					return false
