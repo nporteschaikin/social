cbeb342fcd67ea0032fcf831d9d202cbec4306f8
Noah Portes Chaikin <noah@kingandpartners.com>
2013-08-06 15:33:11 -0400

    DRYing up by removing more

--- db/migrate/20130806161513_create_social_tumblr_texts.rb
+++ db/migrate/20130806161513_create_social_tumblr_texts.rb
@@ -3,7 +3,6 @@ class CreateSocialTumblrTexts < ActiveRecord::Migration
     create_table :social_tumblr_texts do |t|
       t.text :body
       t.string :title
-      t.string :more
     end
   end
 end
