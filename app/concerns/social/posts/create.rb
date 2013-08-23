578e5330caeacec0cb6d23372864eaa2b0ad4e03
Noah Portes Chaikin <noah@kingandpartners.com>
2013-08-23 10:08:48 -0400

    fixed new join statement

--- app/concerns/social/posts/create.rb
+++ app/concerns/social/posts/create.rb
@@ -1,19 +1,26 @@
 module Social
-	module Posts
-		module Create
-			
-			extend ActiveSupport::Concern
-			
-			included do
-			  has_one :parent, as: :post, class_name: "Social::Post"
-			  default_scope { select("#{self.table_name}.*, social_posts.published_at, social_posts.address").joins(:parent) }
-			  attr_accessor :tags_found
-    		
-				after_create do
-					Social::Post.create post: self, published_at: self.published_at, address: self.address, tags_found: self.tags_found
-				end
-			end
-			
-		end
-	end
+  module Posts
+    module Create
+      
+      extend ActiveSupport::Concern
+      
+      included do
+        
+        has_one :parent, as: :post, class_name: "Social::Post", dependent: :destroy
+        default_scope { select("#{self.table_name}.*, social_posts.published_at, social_posts.address").joins(:parent) }
+        attr_accessor :tags_found
+        
+        after_create do
+          Social::Post.create post: self, published_at: self.published_at, address: self.address, tags_found: self.tags_found
+        end
+        
+        def initialize(*args)
+          [:published_at, :address].each {|x| self.class.send(:attr_accessor, x) }
+          super
+        end
+        
+      end
+      
+    end
+  end
 end
