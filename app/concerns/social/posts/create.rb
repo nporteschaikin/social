ab9386231357e5a3222b2d44fb31fa4814c4e106
Noah Portes Chaikin <noah@kingandpartners.com>
2013-08-23 09:28:24 -0400

    slightly reorganized new code

--- app/concerns/social/posts/create.rb
+++ app/concerns/social/posts/create.rb
@@ -6,8 +6,8 @@ module Social
 			
 			included do
 			  has_one :parent, as: :post, class_name: "Social::Post"
-			  attr_accessor :tags_found
 			  default_scope { select("#{self.table_name}.*, social_posts.published_at, social_posts.address").joins(:parent) }
+			  attr_accessor :tags_found
     		
 				after_create do
 					Social::Post.create post: self, published_at: self.published_at, address: self.address, tags_found: self.tags_found
