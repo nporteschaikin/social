0eeb7200b087bfca2e74867c6f86162e35ae02ac
Noah Portes Chaikin <noah@kingandpartners.com>
2013-08-23 10:25:20 -0400

    get parent table_name dynamically in default_scope

--- app/concerns/social/posts/create.rb
+++ app/concerns/social/posts/create.rb
@@ -7,7 +7,7 @@ module Social
       included do
         
         has_one :parent, as: :post, class_name: "Social::Post", dependent: :destroy
-        default_scope { select("#{self.table_name}.*, social_posts.published_at, social_posts.address").joins(:parent) }
+        default_scope { select("#{self.table_name}.*, #{Social::Post.table_name}.published_at, #{Social::Post.table_name}.address").joins(:parent) }
         attr_accessor :tags_found
         
         after_create do
