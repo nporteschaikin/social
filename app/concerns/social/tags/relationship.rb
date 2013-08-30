11c95426eea20f87efbcd64c2147380f0230dbb2
Noah Portes Chaikin <noah@kingandpartners.com>
2013-08-30 10:47:20 -0400

    a tag 'scope' and moved popular tags here

--- app/concerns/social/tags/relationship.rb
+++ app/concerns/social/tags/relationship.rb
@@ -15,6 +15,15 @@ module Social
           self.tags_found.each {|t| self.tag_relationships.create tag: Tag.find_or_create_by_name(t) } if self.tags_found.is_a? Array
         end
         
+        class << self
+          def tag(tag)
+    	      joins(:tags).where(social_tags: {name: tag})
+    	    end
+
+    	    def popular_tags
+    	      unscoped.includes(:tags).collect {|x| x.tags }.flatten.group_by{|x| x}.sort_by{|k, v| -v.size}.map(&:first)
+    	    end
+    	  end
   		end
 
   	end
